d46452d53aec13669a898b2211dd63f66734596c
Kevin Souto <kevinsoutods@gmail.com>
2022-08-06 01:10:22 -0300

    Recebendo os States na GameJSX

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
 import './Game.css'
 
-const Game = ({verifyLetter}) => {
+const Game = ({
+  verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score 
+  }) => {
+
   return (
     <div className='game'>
       <p className='points'>
-        <span>Pontuação: 000</span>
+        <span>Pontuação: {score}</span>
       </p>
       <h1>Adivinha a palavra:</h1>
       <h3 className='tip'>
-        Dica sobre palavra: <span>Dica...</span>
+        Dica sobre palavra: <span>{pickedCategory.toUpperCase()}</span>
       </h3>
+      <p>Você ainda tem {guesses} tentativas</p>
       <div className="wordContainer">
-        <span className='letter'>A</span>
-        <span className="blankSquare"></span>
+        {letters.map((letter, i) => (
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">
+              {letter}
+            </span>
+          ) : (
+            <span key={i} className="blankSquare"></span>
+          )
+        ))}
       </div>
       <div className="letterContainer">
         <p>Tente advinhar uma letra da palavra:</p>
@@ -24,8 +42,9 @@ const Game = ({verifyLetter}) => {
       </div>
       <div className="wrongLetterContainer">
         <p>Letras já utilizadas:</p>
-        <span>a,</span>
-        <span>b,</span>
+            {wrongLetters.map((letter, i) => {
+              <span key={i}>{letter}, </span>
+            })}
       </div>
     </div>
   )
