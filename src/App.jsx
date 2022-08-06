ddf3ba3357665f77caaa5c80c060f9e64332f708
Kevin Souto <kevinsoutods@gmail.com>
2022-08-06 00:37:53 -0300

    Resgatando um categoria e palavra aleatoria

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,50 @@ const stages = [
 ]
 
 function App() {
+
+
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState("");
+
+
+  const pickWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words)
+    const category = 
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    //console.log(category);
+
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    //console.log(word);
+
+    return {word, category};
+  }
+
   // starts the secret word game
   const startGame = () => {
+    //pick word and pick category
+    const {word, category} = pickWordAndCategory();
+
+    // create an array of letters
+    let wordLetters = word.split("");
+
+    // transform all letters in lowercase mod
+    wordLetters = wordLetters.map((letter) => letter.toLowerCase());
+
+    //fill states
+    setPickedCategory(category);
+    setPickedWord(word);
+    setLetters(wordLetters);
+
+    console.log(word, category);
+    console.log(wordLetters);
+
     setGameStage(stages[1].name);
   };
 
@@ -36,6 +75,8 @@ function App() {
   const returnHome = () => {
     setGameStage(stages[0].name);
   }
+
+
 
   return (
     <div className="App">
