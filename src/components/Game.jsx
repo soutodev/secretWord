f819ecbd372c687faf16bbc381ffdab82a3eac27
Kevin Souto <kevinsoutods@gmail.com>
2022-08-06 00:43:44 -0300

    Tela Game JSX

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,9 +3,30 @@ import './Game.css'
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar o jogo</button>
+    <div className='game'>
+      <p className='points'>
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinha a palavra:</h1>
+      <h3 className='tip'>
+        Dica sobre palavra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className='letter'>A</span>
+        <span className="blanckSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required/>
+          <button>Jogar</button>
+        </form>
+      </div>
+      <div className="wrongLetterContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
     </div>
   )
 }
